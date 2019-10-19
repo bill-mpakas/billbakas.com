5859aac3e8caa6ea11428e59297d4ba5c5e71685
Vasileios Bakas <bill.bakas92@gmail.com>
2019-10-19 12:30:44 +0300

    Layout Styling

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,22 +1,6 @@
 <template>
   <div class="font-sans text-gray-800 leading-normal flex flex-col min-h-screen">
-    <header class="border-t-14 border-green-700">
-      <nav class="container mx-auto flex flex-wrap justify-between items-center py-8">
-        <div>
-          <g-link to="/"><g-image src="../../static/logo.svg" class="w-40" alt="logo" /></g-link>
-        </div>
-        <div class="block lg:hidden">
-          <button @click="toggle" class="flex items-center px-3 py-2 border rounded border-gray-500 hover:text-gray-600 hover:border-gray-600">
-            <svg class="current-color h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" fill="gray" /></svg>
-          </button>
-        </div>
-        <ul class="uppercase tracking-wide font-bold w-full block flex-grow lg:flex lg:flex-initial lg:w-auto items-center mt-8 lg:mt-0"
-          :class="isOpen ? 'block': 'hidden'">
-          <li class="mr-8 mb-6 lg:mb-0"><a href="#" class="text-gray-800 hover:text-gray-600">Projects</a></li>
-          <li class="mr-8 mb-6 lg:mb-0"><a href="/blog" class="text-gray-800 hover:text-gray-600">Blog</a></li>
-        </ul>
-      </nav>
-    </header>
+    <Navbar />
     <div class="flex-grow">
       <slot/>
     </div>
@@ -41,10 +25,10 @@ query {
 </style>
 
 <script>
-
+import Navbar from '../components/Navbar'
 export default {
   components: {
-    
+    Navbar
   },
   mounted() {
     
