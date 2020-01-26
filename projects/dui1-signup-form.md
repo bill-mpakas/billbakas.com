749535edaab7af8720bf6737ba790cde36ac3f51
Vasileios Bakas <bill.bakas92@gmail.com>
2020-01-26 17:36:48 +0200

    Added first project, vue feather icons

---
+++ projects/dui1-signup-form.md
@@ -0,0 +1,43 @@
+---
+title: Daily Ui Challenge Day-1
+date: 2019-01-26
+--- 
+
+
+import Signup from '~/components/dui/signup-form.vue'
+
+<Signup />
+
+# Introducing 
+
+Hi! I'm your first Markdown file in **StackEdit**. If you want to learn about StackEdit, you can read me. If you want to play with Markdown, you can edit me. Once you have finished with me, you can create new files by opening the **file explorer** on the left corner of the navigation bar.
+
+   ```html
+<div class="flex justify-start bg-gray-200">
+ <div class="text-gray-700 text-center bg-gray-400 px-4 py-2 m-2">1</div>
+  <div class="text-gray-700 text-center bg-gray-400 px-4 py-2 m-2">2</div>
+  <div class="text-gray-700 text-center bg-gray-400 px-4 py-2 m-2">3</div>
+</div>
+```
+
+```javascript
+var s = "JavaScript syntax highlighting";
+alert(s);
+```
+ 
+```python
+s = "Python syntax highlighting"
+print s
+```
+ 
+```
+No language indicated, so no syntax highlighting. 
+But let's throw in a <b>tag</b>.
+```
+
+
+
+
+
+
+
---
+++ src/components/dui/signup-form.vue
@@ -0,0 +1,51 @@
+<template>
+  <div id="signup">
+    <div class="flex m-4">
+      <div class="w-1/2 bg-gray-200 p-3">
+        <div class="inline-flex align-items-center mb-2">
+          <check-icon size="2x" class="custom-class"></check-icon>
+          <div class="text-md">We’re animal lovers backed by insurance and experience</div>
+        </div>
+        <div class="inline-flex align-items-center mb-2">
+          <check-icon size="2x" class="custom-class"></check-icon>
+          <div class="text-md">We’re animal lovers backed by insurance and experience</div>
+        </div>
+        <div class="inline-flex align-items-center mb-2">
+          <check-icon size="2x" class="custom-class"></check-icon>
+          <div class="text-md">We’re animal lovers backed by insurance and experience</div>
+        </div>
+        <div class="inline-flex align-items-center mb-2">
+          <check-icon size="2x" class="custom-class"></check-icon>
+          <div class="text-md">We’re animal lovers backed by insurance and experience</div>
+        </div>
+      </div>
+      <div class="w-1/2 bg-gray-500 h-12">
+          Other Container
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import { CheckIcon } from 'vue-feather-icons'
+  export default {
+    data() {
+      return {
+        show: true
+      }
+    },
+    components: {
+      CheckIcon
+    },
+  }
+</script>
+
+<style scoped>
+  /*svg.feather {*/
+  /*  height: 1em;*/
+  /*  margin-top: -4px;*/
+  /*  pointer-events: none;*/
+  /*  vertical-align: middle;*/
+  /*  width: 1em;*/
+  /*}*/
+</style>
