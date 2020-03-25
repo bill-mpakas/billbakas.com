6975222181c6f5af5130ccf46d2f4bfee5904c5a
Vasileios Bakas <bill.bakas92@gmail.com>
2020-03-25 11:40:17 +0200

    Design Commit

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -4,6 +4,7 @@
       <span>Hey,there </span>
       <h1 class="text-3xl">I am Bill Bakas</h1>
       <h2 class="text-2xl">Full Stack Designer</h2>
+      <svg viewBox="0 0 20 20" fill="currentColor" class="w-8 h-8"><path fill-rule="evenodd" d="M18 13V5a2 2 0 00-2-2H4a2 2 0 00-2 2v8a2 2 0 002 2h3l3 3 3-3h3a2 2 0 002-2zM5 7a1 1 0 011-1h8a1 1 0 110 2H6a1 1 0 01-1-1zm1 3a1 1 0 100 2h3a1 1 0 100-2H6z" clip-rule="evenodd"></path></svg>
     </section>
     <section id="about-me">
       <div class="flex flex-col sm:flex-row justify-between items-center mb-16">
@@ -13,7 +14,6 @@
     </section>
 
     <section id="projects">
-      
       <g-link to="/projects/">More Projects</g-link>
     </section>
 
