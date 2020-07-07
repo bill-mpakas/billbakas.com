487d751557d117428d3ddda3553a1041770ab63a
Bill Pointer <vasilis.pointer@gmail.com>
2020-07-07 14:02:16 +0300

    Fixed broken query

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -3,12 +3,27 @@
     <Hero />
     <section id="blog">
       <div v-for="edge in $page.posts.edges" :key="edge.node.id">
-      <g-link :to="edge.node.path">{{ edge.node.title }}</g-link>
-    </div>
+        <g-link :to="edge.node.path">{{ edge.node.title }}</g-link>
+      </div>
     </section>
 
   </Layout>
 </template>
+
+<page-query>
+  query Posts {
+    posts: allPost {
+      edges {
+        node {
+          id
+          title
+          path
+                }
+          }
+        }
+      }
+</page-query>
+
 
 <script>
 import Hero from "../components/Hero";
